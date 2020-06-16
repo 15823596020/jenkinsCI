2cb68e55486e059868a5564272ed78c57b39b10c
ytq <ytq12875@163.com>
2020-06-16 23:21:51 +0800

    submit

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 killPython()
 {
     pid=`ps -ef | grep "python_order/orderServer.py" | grep -v grep | awk '{print $2}'`
-    echo "python Id list :$pid"
+    echo "python server Id list :$pid"
     if [ "$pid" = "" ]
     then
-      echo "no python pid alive"
+      echo "no python server pid alive"
     else
       kill -9 $pid
     fi
