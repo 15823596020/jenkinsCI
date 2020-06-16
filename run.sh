e05bd835505b9d3eea4c7932bcf6d86a2be4849f
ytq <ytq12875@163.com>
2020-06-16 16:42:41 +0800

    提交代码

---
+++ run.sh
@@ -0,0 +1,16 @@
+killPython()
+{
+    pid=`ps -ef | grep "python_order/orderServer.py" | grep -v grep | awk '{print $2}'`
+    echo "python Id list :$pid"
+    if [ "$pid" = "" ]
+    then
+      echo "no python pid alive"
+    else
+      kill -9 $pid
+    fi
+}
+
+killPython
+cd $WORKSPACE/python_order
+python3 orderServer.py &
+sleep 5
